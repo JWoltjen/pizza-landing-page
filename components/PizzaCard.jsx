35b7a96b7cde4a747054bd95beca366bb1564c05
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 13:31:21 -0600

    fill out pizza card with stock image, change z-index of navbar

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from '../styles/PizzaCard.module.css'
+import Image from 'next/image'
 
 function PizzaCard() {
     return (
         <div className={styles.container}>
-            Pizza
+            <Image src='/img/pizza.png' alt="pizza" width="500" height="500"/>
+            <h1 className={styles.title}>FIORI DI ZUCCA</h1>
+            <span className={styles.price}>$19.99</span>
+            <p className={styles.desc}>
+                Tempor in ut ad sint fugiat. Tempor ea aliquip aliquip velit dolore culpa deserunt cillum laboris consequat qui. Pariatur sint cillum mollit ipsum magna exercitation officia commodo proident sit. Et et id ad sunt aliqua proident eiusmod ullamco cillum culpa. Culpa eu consequat culpa magna voluptate sint qui aliqua. Duis minim sunt sunt exercitation nostrud consectetur.
+            </p>
         </div>
     )
 }
