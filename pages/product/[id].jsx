9f4334d8228db5103ba95dabdd78a839e0c8af2a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 16:06:04 -0600

    add pizza element and style same

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -4,8 +4,23 @@ import Image from 'next/image';
 
 
 function Product () {
+    const pizza = {
+        id: 1, 
+        img: "/img/pizza.png", 
+        name: "PEPPERONI", 
+        price: [19.99, 23.99, 27.99], 
+        desc: "Adipisicing enim esse adipisicing adipisicing culpa laborum fugiat irure. Sint duis non magna et irure."
+    }
     return (
-        <div className={styles.container}></div>
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <div className={styles.imgContainer}>
+                    <Image alt="" src={pizza.img} layout="fill"/>
+                </div>
+            </div>
+            <div className={styles.right}></div>
+
+        </div>
     )
 }
 
