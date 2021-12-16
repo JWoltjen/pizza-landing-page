b0247bfef076833c72b71e87d5f6d2bfe25ff1bd
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 16:02:13 -0600

    create [id] page

---
+++ pages/product/[id].jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Product.module.css';
+import Image from 'next/image'; 
+
+
+function Product () {
+    return (
+        <div className={styles.container}></div>
+    )
+}
+
+export default Product 
