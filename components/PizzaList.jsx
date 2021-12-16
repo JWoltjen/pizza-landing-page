498e8117b326bd0f6df2994e811353b99e5d0400
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 13:20:19 -0600

    initial styling for pizza list

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -7,9 +7,13 @@ function PizzaList() {
         <div className={styles.container}>
             <h1> The Best Pizza in Town!!</h1>
             <p className={styles.desc}>
-                Dolor tempor laborum reprehenderit commodo nisi magna aute labore dolore laboris fugiat ex. Commodo qui esse ut do nulla sit nostrud. Dolore eiusmod laboris consequat ut nisi in do. Est culpa non non est laboris irure aliquip id ullamco duis incididunt exercitation.
+                Dolor tempor laborum reprehenderit commodo nisi magna aute labore dolore laboris fugiat ex. Commodo qui esse ut do nulla sit nostrud. Dolore eiusmod laboris consequat ut nisi in do. Est culpa non non est laboris irure aliquip id ullamco duis incididunt exercitationd.
             </p>
             <div className={styles.wrapper}>
+                <PizzaCard/>    
+                <PizzaCard/>    
+                <PizzaCard/>    
+                <PizzaCard/>    
                 <PizzaCard/>    
             </div>
         </div>
