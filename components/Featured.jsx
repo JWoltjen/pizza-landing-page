64f57300732053e46baabd61d5dd7a9b642d4b61
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 12:52:44 -0600

    add useState and click handle function for arrow slider logic

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/Featured.module.css'
 import Image from 'next/image'
 
 
 function Featured() {
+    const [index, setIndex] = useState(0)
 
     const images = [
         "/img/featured.png",
         "/img/featured2.png",
         "/img/featured3.png",
     ];
-
+    const handleArrow = (direction) => {
+        if(direction==="l"){
+            setIndex(index !== 0  ? index-1 : 2)
+        }
+        if(direction==="r"){
+            setIndex(index !== 2 ? index+1 :0)
+        }
+    }
     return (
         <div className={styles.container}>
-            <div className={styles.arrowContainer} style={{left:0}}>
+            <div className={styles.arrowContainer} style={{left:0}} onClick={() =>handleArrow("l")}>
             <Image src="/img/arrowl.png" alt="" layout="fill" />
             </div>
             <div className={styles.wrapper}>
@@ -24,7 +32,7 @@ function Featured() {
                     ))}
                 
             </div>
-            <div className={styles.arrowContainer} style={{right:0}}>
+            <div className={styles.arrowContainer} style={{right:0}} onClick={() =>handleArrow("r")}>
                 <Image src="/img/arrowr.png" layout='fill' alt ="" />
             </div>
         </div>
