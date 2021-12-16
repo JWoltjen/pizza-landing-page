c73664dc23a7f7a6b9ed92794b6b8aa580f73b9f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-16 13:18:57 -0600

    create pizzaList and pizzaCard components

---
+++ components/PizzaCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PizzaCard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PizzaCard
---
+++ components/PizzaList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from '../styles.PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+function PizzaList() {
+    return (
+        <div className={styles.container}>
+            <h1> The Best Pizza in Town!!</h1>
+            <p className={styles.desc}>
+                Dolor tempor laborum reprehenderit commodo nisi magna aute labore dolore laboris fugiat ex. Commodo qui esse ut do nulla sit nostrud. Dolore eiusmod laboris consequat ut nisi in do. Est culpa non non est laboris irure aliquip id ullamco duis incididunt exercitation.
+            </p>
+            <div className={styles.wrapper}>
+                <PizzaCard/>    
+            </div>
+        </div>
+    )
+}
+
+
+export default PizzaList
