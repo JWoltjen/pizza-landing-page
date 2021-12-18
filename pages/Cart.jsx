2f91e5325c73ca50674d079bdc57dee9d607555e
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-17 19:42:47 -0600

    final branch commit

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -14,10 +14,52 @@ function Cart() {
                         <th>Quantity</th>
                         <th>Total</th>
                     </tr>
+                    <tr className={styles.tr}>
+                        <td>
+                            <div className={styles.imgContainer}>
+                                <Image 
+                                    src="/img/pizza.png" 
+                                    height='100'
+                                    width='100'
+                                    objectFit="cover" 
+                                    alt="pizza"
+                                />
+                            </div>
+                        </td>
+                        <td>
+                            <span className={styles.name}>SUPREME</span>
+                        </td>
+                        <td>
+                            <span className={styles.extra}>
+                                Double ingredient, spicy sauce
+                            </span>
+                        </td>
+                        <td>
+                            <span className={styles.price}>$19.91</span>
+                        </td>
+                        <td>
+                            <span className={styles.quantity}>2</span>
+                        </td>
+                        <td>
+                            <span className={styles.total}>$39.88</span>
+                        </td>
+                    </tr>
                 </table>
             </div>
             <div className={styles.right}>
-
+                <div className={styles.wrapper}>
+                    <h2 className={styles.title}>Cart Total</h2>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextFile}>Subtotal:</b>$79.60
+                    </div>
+                    <div className={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                    </div>
+                    <div classname={styles.totalText}>
+                        <b className={styles.totalTextTitle}>Total</b>$79.60
+                    </div>
+                    <button className={styles.button}>CHECKOUT NOW!</button>
+                </div>
             </div>
         </div>
     )
