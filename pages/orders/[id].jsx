a1d6fb52720060eaca53ef33df0a4a249e340cc4
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-20 10:11:50 -0600

    create order files in pages and css modules

---
+++ pages/orders/[id].jsx
@@ -0,0 +1,14 @@
+import styles from '../../styles/Order.module.css'
+import Image from 'next/image'
+
+
+const Order = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}></div>
+            <div className={styles.right}></div>
+        </div>
+    )
+}
+
+export default Order
