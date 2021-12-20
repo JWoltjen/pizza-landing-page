b1e0fdae3a1d1221e6153b7ba2d21e2c671e7652
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-20 12:24:40 -0600

    style pizza list and pizza card for mobile 480px

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -9,7 +9,7 @@ function PizzaCard() {
             <h1 className={styles.title}>FIORI DI ZUCCA</h1>
             <span className={styles.price}>$19.99</span>
             <p className={styles.desc}>
-                Tempor in ut ad sint fugiat. Tempor ea aliquip aliquip velit dolore culpa deserunt cillum laboris consequat qui. Pariatur sint cillum mollit ipsum magna exercitation officia commodo proident sit. Et et id ad sunt aliqua proident eiusmod ullamco cillum culpa. Culpa eu consequat culpa magna voluptate sint qui aliqua. Duis minim sunt sunt exercitation nostrud consectetur.
+                Tempor in ut ad sint fugiat. Tempor ea aliquip aliquip velit dolore culpa deserunt cillum laboris consequat qui.  
             </p>
         </div>
     )
