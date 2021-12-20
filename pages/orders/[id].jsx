9bc443b6ad933e1371759f3e619ea7aa24c6167f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-12-20 10:15:12 -0600

    paste cart elements into right container of order page

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -1,12 +1,25 @@
 import styles from '../../styles/Order.module.css'
 import Image from 'next/image'
 
-
 const Order = () => {
     return (
         <div className={styles.container}>
             <div className={styles.left}></div>
-            <div className={styles.right}></div>
+            <div className={styles.right}>
+                <div className={styles.wrapper}>
+                        <h2 className={styles.title}>Cart Total</h2>
+                        <div className={styles.totalText}>
+                            <b className={styles.totalTextFile}>Subtotal:</b>$79.60
+                        </div>
+                        <div className={styles.totalText}>
+                            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+                        </div>
+                        <div classname={styles.totalText}>
+                            <b className={styles.totalTextTitle}>Total</b>$79.60
+                        </div>
+                        <button className={styles.button}>CHECKOUT NOW!</button>
+                </div>
+            </div>
         </div>
     )
 }
